3d2b01f62a44573475436d23f4eedf62307f84d3
Oliver2081 <oliver.spilsted@btinternet.com>
2025-11-10 18:34:24 +0000

    Enhance webhook for GitHub event handling
    Refactor webhook to include signature validation and handle GitHub events properly. Update the server only on push events to the master branch.

--- backend.py
+++ backend.py
@@ -1,21 +1,80 @@
-import os
-import json
 import git
+import os, json
+import hashlib, hmac
+from dotenv import load_dotenv
 from datetime import datetime
 from flask import render_template, Flask, abort, request
 
 app = Flask(__name__)
+load_dotenv()
+
 DATAPATH = '/home/o2081/website/data'
+w_secret = os.environ['WEBHOOK_SECRET']
+
+# Functions
+def is_valid_signature(x_hub_signature, data, private_key):
+    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(private_key, 'latin-1')
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature) 
 
 @app.route('/update_server', methods=['POST'])
 def webhook():
-    if request.method == 'POST':
-        repo = git.Repo('website')
-        origin = repo.remotes.origin
-        origin.pull()
-        return 'Updated PythonAnywhere successfully', 200
+    if request.method != 'POST':
+        return 'OK'
     else:
-        return 'Wrong event type', 400
+        abort_code = 400
+        # Do initial validations on required headers
+        if 'X-Github-Event' not in request.headers:
+            abort(abort_code)
+        if 'X-Github-Delivery' not in request.headers:
+            abort(abort_code)
+        if 'X-Hub-Signature' not in request.headers:
+            abort(abort_code)
+        if not request.is_json:
+            abort(abort_code)
+        if 'User-Agent' not in request.headers:
+            abort(abort_code)
+        ua = request.headers.get('User-Agent')
+        if not ua.startswith('GitHub-Hookshot/'):
+            abort(abort_code)
+
+        event = request.headers.get('X-GitHub-Event')
+        if event == "ping":
+            return json.dumps({'msg': 'Hi!'})
+        if event != "push":
+            return json.dumps({'msg': "Wrong event type"})
+
+        x_hub_signature = request.headers.get('X-Hub-Signature')
+        # webhook content type should be application/json for request.data to have the payload
+        # request.data is empty in case of x-www-form-urlencoded
+        if not is_valid_signature(x_hub_signature, request.data, w_secret):
+            print('Deploy signature failed: {sig}'.format(sig=x_hub_signature))
+            abort(abort_code)
+
+        payload = request.get_json()
+        if payload is None:
+            print('Deploy payload is empty: {payload}'.format(payload=payload))
+            abort(abort_code)
+
+        if payload['ref'] != 'refs/heads/master':
+            return json.dumps({'msg': 'Not master; ignoring'})
+            
+        repo = git.Repo('/home/o2081/website')
+        origin = repo.remotes.origin
+
+        pull_info = origin.pull()
+
+        if len(pull_info) == 0:
+            return json.dumps({'msg': "Didn't pull any information from remote!"})
+        if pull_info[0].flags > 128:
+            return json.dumps({'msg': "Didn't pull any information from remote!"})
+
+        commit_hash = pull_info[0].commit.hexsha
+        build_commit = f'build_commit = "{commit_hash}"'
+        print(f'{build_commit}')
+        return 'Updated PythonAnywhere server to commit {commit}'.format(commit=commit_hash)
 
 @app.route("/")
 def index():
@@ -31,7 +90,7 @@ def renderPage(pageId):
     availablePages = os.listdir(DATAPATH)
     
     for page in availablePages:
-        if page[-5:] == ".json":
+        if page.endswith(".json"):
             validPages.append(page[:-5])
 
     if pageId in validPages:
@@ -42,7 +101,7 @@ def renderPage(pageId):
             SPECREF = pageData.get("specRef")
             SECTIONS = []
             
-            for section in pageData.get("sections"):
+            for section in pageData.get("sections", []):
                 SECTIONS.append(section)
 
         return render_template("page.html", title=TITLE, specRef=SPECREF, sections=SECTIONS)
@@ -53,15 +112,4 @@ def renderPage(pageId):
 @app.errorhandler(404)
 def pageNotfound(error):
     return render_template('404.html'), 404
-
-
-
-
-
-
-
-
-
-
-
-
+    
